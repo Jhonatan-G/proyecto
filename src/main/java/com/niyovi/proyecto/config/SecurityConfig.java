617d852216060ace559cb3a4793dec02614292f0
Jhonatan <jhonatangamba10@gmail.com>
2025-04-23 16:59:55 -0500

    Dependencias y log

--- src/main/java/com/niyovi/proyecto/config/SecurityConfig.java
+++ src/main/java/com/niyovi/proyecto/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.niyovi.proyecto.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +17,7 @@ import org.springframework.security.web.header.writers.CacheControlHeadersWriter
 public class SecurityConfig {
 
     @Autowired
+    @Lazy
     private UserDetailsService userDetailsService;
 
     @Autowired
